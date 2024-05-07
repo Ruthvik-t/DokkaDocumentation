7b280609d52a2d28ca5296890b543b652cdfd2cf
Ruthvik Thiriveedhi <Ruthvik.Thiriveedhi@tesco.com>
2024-05-07 18:49:35 +0530

    Initial project setup with a library module

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "DokkaDocumentation"
+include(":app")
+include(":library-1")
